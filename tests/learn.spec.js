b58d605fe683f3fb992ad48b68fda971d6338ba6
mohannagarajan16 <mohannagarajan16@gmail.com>
2026-06-11 11:46:03 +0530

    first commit playwright

---
+++ tests/learn.spec.js
@@ -0,0 +1,25 @@
+const {test, expect}= require('@playwright/test');
+
+test('First test',async ({page})=>{
+    await page.goto('https://www.google.com/');
+    console.log(await page.title());
+   // await page.locator('input[name="q"]').type('Playwright');
+   // await page.locator('input[name="btnK"]').click();
+   // await page.waitForTimeout(2000);
+})
+
+test('Second test',async ({page})=>{
+    await page.goto('https://login.yahoo.com/');
+    await expect(page).toHaveTitle(`Log-in-Sign in to Login'`);
+    
+    
+})
+ 
+test('Third test' ,async({page})=>{
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    await page.locator('#username').type('rahulshettyacademy');
+    await page.locator('#password').type('learning');
+    await page.locator('#signInBtn').click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+});
---
+++ tests/test.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test.only('first login test', async ({ page }) => {
+
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    await page.locator('#username').fill('rahulshettyacademy');
+    await page.locator('#password').fill('Learning@830$3mK2');
+    await page.locator('#signInBtn').click();  
+    await expect(page.title()).toContainText('login page practise | Rahul Shetty Academy');
+
+  // test steps go here
+});
+
+
